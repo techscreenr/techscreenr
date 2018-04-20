25ad9b7edc4dd21d930211f3504cdda534c3857b
Michael Taylor <michael.taylor5051@gmail.com>
2018-04-19 22:51:45 -0600

    Implemented Markup for Question Examples

--- techscreenr-angular/client/app/questions/question/question.component.ts
+++ techscreenr-angular/client/app/questions/question/question.component.ts
@@ -5,7 +5,8 @@ import { Question, Answer, QuestionTypeEnum } from '../../../../models';
 
 @Component({
   selector: 'app-question',
-  templateUrl: './question.component.html'
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent {
     @Input() activeQuestion: Question;
